e189670dcc1fe1fd3be0fb5f870b8738472de83c
Ujjwal Suman <ujjwalSuman007@gmail.com>
2021-06-24 12:38:12 +0530

    remove api for search character

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { Box } from '@material-ui/core';
 import Header from './components/Header';
-import { getcharacters, getSearchedcharacters } from './service/character';
+import { getcharacters } from './service/character';
 import Characters from './components/Characters';
 
 const App = (props) => {
   const { characters, isLoading } = props;
+  const [data, setData] = useState([]);
   useEffect(() => {
     props.getcharacters();
   }, []);
 
+  useEffect(() => {
+    setData(characters);
+  }, [characters]);
+
   const getText = (text) => {
-    props.getSearchedcharacters(text);
+    setData(characters);
+    if (text.length !== 0) {
+      const filteredCharacter = characters.filter((el) => {
+        const name = el.name.toLowerCase();
+        const getData = name.includes(text.toLowerCase());
+        return getData;
+      });
+      setData(filteredCharacter);
+    } else {
+      setData(characters);
+    }
   };
 
   return (
     <Box>
       <Header getText={getText} />
-      <Characters data={characters} isLoading={isLoading} />
+      <Characters data={data} isLoading={isLoading} />
     </Box>
   );
 };
@@ -31,7 +46,6 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   getcharacters: () => dispatch(getcharacters()),
-  getSearchedcharacters: (text) => dispatch(getSearchedcharacters(text)),
 });
 
 export default compose(connect(mapStateToProps, mapDispatchToProps))(App);
--- src/service/character.js
+++ src/service/character.js
@@ -1,6 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const initialState = {
+  characters: [],
+  isLoading: false,
+};
+
 export const getcharacters = createAsyncThunk(
   'CHARACTERS/GET_ALL_CHARACTERS',
   async () => {
@@ -8,21 +13,6 @@ export const getcharacters = createAsyncThunk(
     return data.data;
   },
 );
-
-export const getSearchedcharacters = createAsyncThunk(
-  'CHARACTERS/SEARCH_CHARACTERS',
-  async (text) => {
-    const data = await axios.get(
-      `https://breakingbadapi.com/api/characters?name=${text}`,
-    );
-    return data.data;
-  },
-);
-
-const initialState = {
-  characters: [],
-  isLoading: false,
-};
 
 export const characterslice = createSlice({
   name: 'blog',
@@ -37,17 +27,6 @@ export const characterslice = createSlice({
       state.characters = action.payload;
     },
     [getcharacters.rejected]: (state) => {
-      state.isLoading = false;
-      state.characters = [];
-    },
-    [getSearchedcharacters.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [getSearchedcharacters.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.characters = action.payload;
-    },
-    [getSearchedcharacters.rejected]: (state) => {
       state.isLoading = false;
       state.characters = [];
     },
